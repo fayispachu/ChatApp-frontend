23c74ef31f4d40f64d9c414d3942ccab8f87af1a
Fayis <fayizpachu217@gmail.com>
2025-12-21 22:55:34 +0530

    index basic seo

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const connectSocket = () => {
-  return io("http://localhost:5000/api", {
+  return io("https://chatapp-backend-p8q9.onrender.com", {
     auth: { token: localStorage.getItem("token") },
   });
 };
